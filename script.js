170730a14400f33aee3a44c8bb9526d7589647a1
lchen104 <lchen@zdmedia.com>
2023-06-21 15:22:44 -0400

    Starter Code Project - Forth Commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const addBtn = document.getElementById('add');
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if (notes) {
+    notes.forEach(note => addNewNote(note));
+}
+
 // addBtn.addEventListener('click', ()=> addNewNote('Add new note'));
 addBtn.addEventListener('click', ()=> addNewNote());
 
@@ -24,6 +30,7 @@ function addNewNote(text = '') {
     // Delete Button
     const deleteBtn = note.querySelector('.delete');
 
+    // Edit Button
     const editBtn = note.querySelector('.edit');
 
     const main = note.querySelector('.main');
@@ -36,17 +43,53 @@ function addNewNote(text = '') {
     // Delete Function
     deleteBtn.addEventListener('click', ()=> {
         note.remove();
+        updateLS();
     });
 
 
-    // edit btn
+    // Edit Dunction
     editBtn.addEventListener('click', ()=> {
         main.classList.toggle('hidden');
         textArea.classList.toggle('hidden');
+    })
 
+    // textarea
+    textArea.addEventListener('input', (e)=> {
+        // console.log(e);
+        const value = e.target.value;
+        // console.log(value);
+        main.innerHTML = value;
+
+        updateLS()
     })
 
 
     document.body.appendChild(note);
+
 }
 
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+    // console.log(notesText);
+
+    const notes = [];
+
+    notesText.forEach(note => notes.push(note.value));
+
+    // console.log(notes, notesText);
+
+    localStorage.setItem('notes', JSON.stringify(notes));
+
+}
+
+
+// updateLS();
+
+
+// local storage
+// localStorage.setItem('name', 'Leo');
+// localStorage.getItem('name');
+// localStorage.removeItem('name');
+
+// localStorage.setItem('studentNames', JSON.stringify());
