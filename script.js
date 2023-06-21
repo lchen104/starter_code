2367fd7e3fc3d7ff6d8fc2d84c337d9130b52174
lchen104 <lchen@zdmedia.com>
2023-06-21 13:47:48 -0400

    Starter Code Project - Third Commit

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 const addBtn = document.getElementById('add');
 
-addBtn.addEventListener('click', ()=> addNewNote('Add new note'));
+// addBtn.addEventListener('click', ()=> addNewNote('Add new note'));
+addBtn.addEventListener('click', ()=> addNewNote());
 
-function addNewNote() {
+function addNewNote(text = '') {
     const note = document.createElement('div');
     note.classList.add('note');
 
     note.innerHTML = `
-        <!-- Tool Bar -->
         <div class="tools">
-            <!-- Edit Button -->
             <button class="edit">
                 <i class="fas fa-edit"></i>
             </button>
-            <!-- Delete Button -->.
             <button class="delete">
                 <i class="fas fa-trash-alt"></i>
             </button>
         </div>
 
-        <div class="hidden"></div>
-        <textarea></textarea>
+        <div class="main ${text ? "" : "hidden"}"></div>
+        <textarea class="${text ? "hidden" : ""}"></textarea>
     `;
 
     // Delete Button
     const deleteBtn = note.querySelector('.delete');
-    // delete function
+
+    const editBtn = note.querySelector('.edit');
+
+    const main = note.querySelector('.main');
+
+    const textArea = note.querySelector('textarea');
+    
+    textArea.value = text;
+    main.innerHTML = text;
+
+    // Delete Function
     deleteBtn.addEventListener('click', ()=> {
         note.remove();
     });
+
+
+    // edit btn
+    editBtn.addEventListener('click', ()=> {
+        main.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+
+    })
+
 
     document.body.appendChild(note);
 }
